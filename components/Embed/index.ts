dd6a2449bc0544db331691fd8e01925fbb9600df
Matt Wells <mattwells878@gmail.com>
2022-02-09 20:10:15 -0500

    Fix folder structure

--- components/Embed/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Embed } from "./Embed";
-export { default as EmbedContent } from "./EmbedContent";
-export { default as EmbedButton } from "./EmbedButton";
--- remix-six-mans/app/icons/table/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AscendingIcon } from "./AscendingIcon";
-export { default as DescendingIcon } from "./DescendingIcon";
-export { default as UnsortedIcon } from "./UnsortedIcon";
